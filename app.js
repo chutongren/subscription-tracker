acf3f100b42cf66c3b51d0b8a4fc2f5fbe55e9d5
Chutong Ren <chutong.ren@tum.de>
2025-02-12 10:16:50 +0100

    update

--- app.js
+++ app.js
@@ -5,15 +5,28 @@ import connectToDatabase from "./database/mangodb.js";
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
+import cookieParser from "cookie-parser";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// console.log("✅ authRouter loaded:", authRouter.stack);
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+app.use(errorMiddleware);
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+console.log("PORT:", PORT);
+
 // as soon as we start our application on localhost
 app.listen(PORT, async () => {
   console.log(`Example app listening on port ${PORT}`);
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
+import { signUp } from "../controller/auth.controller.js";
 const authRouter = Router();
-authRouter.post("sign-up", (req, res) => res.send({ title: "Sign Up" }));
-authRouter.post("sign-in", (req, res) => res.send({ title: "Sign In" }));
-authRouter.post("sign-out", (req, res) => res.send({ title: "Sign Out" }));
+authRouter.post("/sign-up", signUp);
+authRouter.get("/sign-in", (req, res) => res.send({ title: "Sign In" }));
+authRouter.post("/sign-out", (req, res) => res.send({ title: "Sign Out" }));
 export default authRouter;
